21cf2f7ea5326f11375660d507bee7ab234d089d
Samuele Perticarari <sperticarari@deloitte.it>
2024-09-24 19:05:53 +0200

    Deleted application. This repo will store only modules.

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "nsg" {
-  value = module.l2_nsg_test.nsg
-  description = "The defined Network Security Group"
-}
-
-output "nsg_rule_allow_http" {
-  value = module.l2_nsg_test.nsg_rule_allow_http
-  description = "The defined Network Security Group rule"
-}
